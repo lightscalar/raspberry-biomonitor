3191ed45afd1573b68a1ca7ed6d621461dd06fd1
Matthew J. Lewis, Ph.D <lightscalar@gmail.com>
2017-08-28 13:30:58 -0400

    Added logos, images, favicon, etc.

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,7 +1,20 @@
 <template>
-  <v-card>
-    <v-icon>timelapse</v-icon>
-  </v-card>
+  <v-parallax src='static/img/heart-logo@2x.png' class='splash'>
+    <v-layout align-center justify-center>
+      <h1 class='logo text--white'>NEW VITAL SIGNS</h1>
+    </v-layout>
+    <v-layout align-center justify-center>
+      <input autofocus maxlength="6" class='code-input' v-model='sessionId'
+         @focus='focusInput'
+         @keyup='keyPress'
+         pattern='[a-zA-Z]{6}' type='text'
+         placeholder='Session ID' name='patientCode'>
+      </input>
+      <v-btn dark primary large class='large-btn' @click.native='loadSession'>
+        <v-icon class='add-patient' large>add</v-icon>
+      </v-btn>
+    </v-layout>
+  </v-parallax>
 </template>
 
 <script>
@@ -15,11 +28,15 @@
 
     data () {
       return {
+        sessionId: ''
 
       }
     },
 
     methods: {
+      keypress () {
+
+      }
 
     },
 
@@ -34,6 +51,39 @@
 
 </script>
 
-<style>
-
+<style scoped>
+.logo {
+  font-size: 40px;
+  letter-spacing: 1em;
+  color: #ffffff;
+  font-weight: bold;
+}
+.code-input {
+  font-size: 40px;
+  border: 4px solid white;
+  color: black;
+  background-color: white;
+  text-transform: uppercase;
+  text-align: center;
+  margin-left: 5px;
+  margin-right: 5px;
+  max-width: 250px;
+  outline: 6px solid #afafaf;
+  padding: 5px;
+}
+.splash {
+    min-height: 1000px;
+}
+.add-patient {
+    color: #305580;
+}
+.container {
+}
+.large-btn {
+    font-size: 50 !important;
+    padding: 5px !important;
+    min-height: 90px !important;
+    background-color: #305580 !important;
+    border: 6px solid #afafaf !important;
+}
 </style>
