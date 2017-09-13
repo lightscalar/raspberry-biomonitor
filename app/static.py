f7d91b952f763452b286694df4889e580442409f
Matthew J. Lewis, Ph.D <lightscalar@gmail.com>
2017-09-13 06:30:16 -0400

    Now using Flask to serve the static site.

--- app/static.py
+++ app/static.py
@@ -1,17 +1,18 @@
 import eventlet
+from eventlet import wsgi
 eventlet.monkey_patch()
-import http.server
-import socketserver
+from flask import Flask, send_from_directory
+from flask_cors import CORS
 import os
 
-# Serve static index.html file from the /site directory.
 PORT = 5000
+app = Flask(__name__, static_folder='../dist/static')
+CORS(app)
+
+@app.route('/')
+def index():
+    return send_from_directory('../dist', 'index.html')
 
 if __name__ == '__main__':
-    web_dir = os.path.join(os.path.dirname(__file__), '../dist')
-    os.chdir(web_dir)
-
-    Handler = http.server.SimpleHTTPRequestHandler
-    httpd = socketserver.TCPServer(("", PORT), Handler)
-    print("serving at port", PORT)
-    httpd.serve_forever()
+    # Run the webserver.
+    app.run()
