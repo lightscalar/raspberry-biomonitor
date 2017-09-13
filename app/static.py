6c0b19ae86d7a95946a8c3f49dc4d609ae7d568f
Matthew J. Lewis, Ph.D <lightscalar@gmail.com>
2017-09-13 07:02:36 -0400

    Windows.

--- app/static.py
+++ app/static.py
@@ -6,12 +6,12 @@ from flask_cors import CORS
 import os
 
 PORT = 5000
-app = Flask(__name__, static_folder='..\dist\static')
+app = Flask(__name__, static_folder='../dist/static', static_url_path='/static')
 CORS(app)
 
 @app.route('/')
 def index():
-    return send_from_directory('..\dist', 'index.html')
+    return send_from_directory('../dist', 'index.html')
 
 if __name__ == '__main__':
     # Run the webserver.
